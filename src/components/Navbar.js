29f05101512c4fd18301ddd9efe54b9e86dbb33f
Nisarg Thakkar <89217455+Nishu0@users.noreply.github.com>
2023-01-22 16:57:34 +0530

    Final Changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { Stack } from '@mui/material'
-import Logo from '../assets/images/Logo.png';
+import Logo from '../assets/Logo.png';
 
 const Navbar = () => {
   return (
